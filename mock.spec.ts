281396d9759c7597077ca448586af3174e90e7ba
Maxim Mazurok <maxim@mazurok.com>
2022-06-27 21:05:52 +1000

    toHaveBeenCalled

--- mock.spec.ts
+++ mock.spec.ts
@@ -1,9 +1,10 @@
 import { jest } from "@jest/globals";
 
 jest.unstable_mockModule("node:fs", () => ({
-  readFileSync: () => "mocked value",
+  readFileSync: jest.fn().mockReturnValue("mocked value"),
 }));
 
+const { readFileSync } = await import("node:fs");
 const { getEngines } = await import("./engines.js");
 
 it("works", () => {
@@ -12,4 +13,5 @@ it("works", () => {
 
   // Assert
   expect(result).toBe("mocked value");
+  expect(readFileSync).toHaveBeenCalled();
 });
