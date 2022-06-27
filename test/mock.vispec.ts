04410204213d63410c01cd51e40acb4dfc8c402a
Maxim Mazurok <maxim@mazurok.com>
2022-06-27 20:59:41 +1000

    toHaveBeenCalled

--- test/mock.vispec.ts
+++ test/mock.vispec.ts
@@ -1,18 +1,20 @@
 import { expect, test, vi } from "vitest";
 import { getEngines } from "../engines.js";
+import { readFileSync } from "node:fs";
+
+// Arrange
+vi.mock("node:fs", async () => {
+  return {
+    ...(await vi.importActual<typeof import("node:fs")>("node:fs")),
+    readFileSync: vi.fn().mockReturnValue(`mocked value`),
+  };
+});
 
 test("works", async () => {
-  // Arrange
-  vi.mock("node:fs", async () => {
-    return {
-      ...(await vi.importActual<typeof import("node:fs")>("node:fs")),
-      readFileSync: () => `mocked value`,
-    };
-  });
-
   // Act
-  const result = await getEngines();
+  const result = getEngines();
 
   // Assert
   expect(result).toBe("mocked value");
+  expect(readFileSync).toHaveBeenCalled();
 });
